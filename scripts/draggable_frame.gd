a221a4ab8e8c77ca2ee45b04b32b1c41a20a76db
MichaelFar <rarrafarrar@gmail.com>
2024-08-09 12:10:47 -0700

    Added inherent support for arbitrarily large frames, fixed bugs associated with dragging frames a certain way

--- scripts/draggable_frame.gd
+++ scripts/draggable_frame.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
+@export var frameSize : int
+
+@export var visualContainer : Node2D
+
 var should_be_dragged := false
 
-var inside_drop_zone := false
+var insideDropZone := false
 
 var body_ref = null
+
+var bodyRefArray : Array
 
 var offset : Vector2
 
@@ -16,14 +22,16 @@ func _ready():
 	pass
 
 func _process(delta):
+	
 	if (should_be_dragged):
 		
 		if(Input.is_action_just_pressed("cast")):
+			
 			initialPosition = global_position
 			offset = get_global_mouse_position() - global_position
 			UiGlobal.dragging_frame = true
 			slotted_in_frame.emit(false, self)
-			
+			#bodyRefArray = []
 		if(Input.is_action_pressed("cast")):
 			
 			global_position = get_global_mouse_position() - offset
@@ -33,56 +41,78 @@ func _process(delta):
 			UiGlobal.dragging_frame = false
 			var tween = get_tree().create_tween()
 			tween.finished.connect(emit_frame_occupied)
-			if(inside_drop_zone):
-				tween.tween_property(self, "position", 
-				body_ref.global_position, 0.2).set_ease(Tween.EASE_OUT)
+			
+			var proper_position = get_proper_position()
+			
+			print("bodyRefArray size is " + str(bodyRefArray.size()))
+			
+			if(bodyRefArray.size() != frameSize):
 				
-			else:
 				tween.tween_property(self, "global_position", 
 				initialPosition, 0.2).set_ease(Tween.EASE_OUT)
 			
+			elif(insideDropZone):
+				
+				tween.tween_property(self, "position", 
+				proper_position, 0.2).set_ease(Tween.EASE_OUT)
+				
+			else:
+				
+				tween.tween_property(self, "global_position", 
+				initialPosition, 0.2).set_ease(Tween.EASE_OUT)
 			
 func _on_area_2d_mouse_entered():
 	
 	if (!UiGlobal.dragging_frame):
 		
 		should_be_dragged = true
-		scale = Vector2(1.05, 1.05)
+		visualContainer.scale = Vector2(1.05, 1.05)
 		
-	#print("Mouse entered")
-	
 func _on_area_2d_mouse_exited():
 	
 	if (!UiGlobal.dragging_frame):
 		
 		should_be_dragged = false
-		scale = Vector2(1.0, 1.0)
+		visualContainer.scale = Vector2(1.0, 1.0)
 		
-	#print("Mouse exited")
-
 func _on_area_2d_body_entered(body):
-	if body.is_in_group("droppable"):
-		if(!body.occupied):
-			slotted_in_frame.connect(body.set_occupied)
-			inside_drop_zone = true
-			body.modulate = Color(Color.REBECCA_PURPLE, 1)
-			body_ref = body
-		
-	#print("Body entered")
 	
+	if body.is_in_group("droppable"):
+		
+		if(!body.occupied):
+			print("body not occupied")
+			slotted_in_frame.connect(body.set_occupied)
+			insideDropZone = true
+			body_ref = body
+			bodyRefArray.append(body_ref)
+			print(bodyRefArray)
+			
 func _on_area_2d_body_exited(body):
 	
 	if(body_ref == body):
 		
-		inside_drop_zone = false
-		body.modulate = body.originalColor
+		insideDropZone = false
 		
 	if(body.is_in_group("droppable")):
 		if(!body.occupied):
-			body.modulate = body.originalColor
-		
-	#print("Body exited")
-
+			print("Popping body ref")
+			bodyRefArray.pop_at(bodyRefArray.find(body))
+	
 func emit_frame_occupied():
+	
 	await get_tree().physics_frame
+	
 	slotted_in_frame.emit(true, self)
+
+func get_proper_position():
+	
+	var averaged_position : Vector2
+	
+	for i in bodyRefArray:
+	
+		averaged_position += i.global_position
+		
+	averaged_position = averaged_position / float(bodyRefArray.size())
+	
+	return averaged_position
+	
--- ui scenes/drop_zone.gd
+++ ui scenes/drop_zone.gd
@@ -6,11 +6,7 @@ var occupied := false
 
 var originalColor = null
 
-#Seeing as we will want to use the occupied boolean to tell if a frame is occupied,
-#this check should probably happen when the action is dragged over the frame
-#meaning we likely shouldn't also set the occupied status at that time
-#this will work for now
-#we should also consider that an occupied frame shouldn't be set to unoccupied if a different frame is dragged over it
+
 
 func _ready():
 	
@@ -18,17 +14,22 @@ func _ready():
 	originalColor = modulate
 
 func _on_area_2d_area_entered(area):
+	
 	print("entered")
+	
 	if area.owner.is_in_group("draggable"):
-		pass
-		#area.owner.slotted_in_frame.connect(set_occupied)
-
-
-func _on_area_2d_area_exited(area):
-	if area.owner.is_in_group("draggable"):
-		if(area.owner.slotted_in_frame.is_connected(set_occupied)):
-			area.owner.slotted_in_frame.disconnect(set_occupied)
 		
+		
+		modulate = Color(Color.REBECCA_PURPLE, 1)
+			
+func _on_area_2d_area_exited(area):
+	
+	if area.owner.is_in_group("draggable"):
+		
+		if(area.owner.slotted_in_frame.is_connected(set_occupied)):
+			
+			area.owner.slotted_in_frame.disconnect(set_occupied)
+		modulate = originalColor
 		#occupied = false
 
 func set_occupied(value : bool, signal_source):
