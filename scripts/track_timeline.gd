2fd04a6b59d5bc2cd3fb176f8612c48fa57b1f00
MichaelFar <rarrafarrar@gmail.com>
2024-08-13 10:27:46 -0700

    Created auto placement logic for track cursor that accounts for an enemy track

--- scripts/track_timeline.gd
+++ scripts/track_timeline.gd
@@ -1,13 +1,18 @@
 extends Node2D
 
-@export var timeLineCursor : Line2D
+@export var timeLineCursor : Area2D
 
 @export var playerTrack : Node2D
 
+@export var enemyTrack : Node2D
+
+@export var activeFrameShapes : Array[CollisionShape2D]
 
 func _ready():
 	
 	set_cursor_start(playerTrack.startPosition)
+	
+	place_cursor()
 	
 func _physics_process(delta):
 	
@@ -22,22 +27,29 @@ func set_cursor_start(start_position : Vector2):
 func tween_cursor_to_end():
 	
 	
-	if(timeLineCursor.global_position == playerTrack.startPosition 
-	|| timeLineCursor.global_position == playerTrack.endPosition):
+	if(timeLineCursor.global_position.x == playerTrack.startPosition.x 
+	|| timeLineCursor.global_position.x == playerTrack.endPosition.x):
+		
+		place_cursor()
 		
 		var tween = get_tree().create_tween()
 		
-		timeLineCursor.global_position = playerTrack.startPosition
-		
-		tween.tween_property(timeLineCursor, "global_position", playerTrack.endPosition, 1.5)
+		tween.tween_property(timeLineCursor, "global_position:x", playerTrack.endPosition.x, 1.5)
 		
 		return tween
 
-
-
-
-
-func _on_area_2d_area_entered(area):
+func _on_area_2d_body_entered(body):
+	
 	print("Body entered")
-	if(area.owner.is_in_group("draggable")):
-		area.owner.create_effect()
+	
+	if(body.is_in_group("draggable")):
+		
+		body.create_effect()
+		
+func place_cursor():
+	
+	activeFrameShapes[0].global_position.y = playerTrack.startPosition.y
+	activeFrameShapes[1].global_position.y = enemyTrack.startPosition.y
+	timeLineCursor.global_position.y = (playerTrack.startPosition.y + enemyTrack.startPosition.y) / 2.0
+		
+	timeLineCursor.global_position.x = playerTrack.startPosition.x
